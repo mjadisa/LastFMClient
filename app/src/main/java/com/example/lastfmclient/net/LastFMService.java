99a54c4deca93b2bd736d408a1977de3d24d50ea
Luke S <user@example.com>
2019-03-07 16:40:47 +0000

    Added data classes for Album Search Results and added Retrofit Service

---
+++ app/src/main/java/com/example/lastfmclient/net/LastFMService.java
@@ -0,0 +1,17 @@
+package com.example.lastfmclient.net;
+
+import com.example.lastfmclient.data.albumResults.AlbumResultsResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface LastFMService {
+
+    @GET(".")
+    Single<AlbumResultsResponse> getAlbumSearchResults(@Query("method") String methodName,
+                                                       @Query("album") String albumName,
+                                                       @Query("limit") int limit,
+                                                       @Query("page") int page,
+                                                       @Query("format") String responseFormat);
+}
