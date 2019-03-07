b5775ee7256389a5a46513a4f0df884f1f2f3cd1
Luke S <user@example.com>
2019-03-07 16:29:55 +0000

    Added Constants

---
+++ app/src/main/java/com/example/lastfmclient/common/Constants.java
@@ -0,0 +1,15 @@
+package com.example.lastfmclient.common;
+
+public class Constants {
+    //API
+    public static final String API_KEY = "sf12mwlry09124bm32s0pqe65nbc098m";
+    public static final String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
+    public static final String QUERY_ALBUM_SEARCH = "album.search";
+    public static final String QUERY_ALBUM_GET_INFO = "album.getInfo";
+    public static final String RESULT_FORMAT = "json";
+    public static final int RESULTS_PER_PAGE = 30;
+    public static final int CACHE_SIZE = 5 * 1024 * 1024; //5 MB
+    public static final int API_TIMEOUT = 30;
+
+
+}
