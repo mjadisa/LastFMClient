8a952eeee4c430d6f5f043222731d2aba5d0a5c3
Luke S <user@example.com>
2019-03-08 17:42:37 +0000

    Added click handling for homescreen

--- app/src/main/java/com/example/lastfmclient/screens/albumDetails/AlbumDetailsViewModelFactory.java
+++ app/src/main/java/com/example/lastfmclient/screens/albumDetails/AlbumDetailsViewModelFactory.java
@@ -1,12 +1,10 @@
 package com.example.lastfmclient.screens.albumDetails;
 
-import android.app.Application;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
 
 import com.example.lastfmclient.data.repo.DataSource;
-import com.example.lastfmclient.screens.home.HomeViewModel;
 
 public class AlbumDetailsViewModelFactory implements ViewModelProvider.Factory {
     private final DataSource lastFMRepository;
@@ -21,7 +19,7 @@ public class AlbumDetailsViewModelFactory implements ViewModelProvider.Factory {
     @SuppressWarnings("unchecked")
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         if (modelClass.isAssignableFrom(AlbumDetailsViewModel.class)) {
-            return  (T) new AlbumDetailsViewModel(lastFMRepository);
+            return (T) new AlbumDetailsViewModel(lastFMRepository);
         }
         throw new IllegalArgumentException("The class has to be an instance of: "
                 + AlbumDetailsViewModel.class.getSimpleName());
